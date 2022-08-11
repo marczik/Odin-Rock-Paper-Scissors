4848b957df8a02a826f4990401e33567bc41dffa
marczik <marek99928@wp.pl>
2022-08-11 17:06:09 +0200

    Write function playRound

--- javascript.js
+++ javascript.js
@@ -15,11 +15,41 @@ Create function game
 
 function getComputerChoice() {
 	let choice = Math.floor(Math.random() * 3) + 1;
-	if ((choice === 1)) {
-		return "Kamień";
-	} else if ((choice === 2)) {
-		return "Papier";
+	if (choice === 1) {
+		return "kamień";
+	} else if (choice === 2) {
+		return "papier";
 	} else {
-		return "Nożyce";
+		return "nożyce";
 	}
 }
+
+function playRound(playerSelection, computerSelection) {
+	playerSelection = playerSelection.toLowerCase();
+
+	if (playerSelection === "kamień" && computerSelection === "nożyce") {
+		return "You win! Kamień pokonuje nożyce.";
+	} else if (playerSelection === "kamień" && computerSelection === "papier") {
+		return "You lose! Papier pokonuje kamień.";
+	} else if (playerSelection === "kapier" && computerSelection === "kamień") {
+		return "You win! Papier pokonuje kamień.";
+	} else if (playerSelection === "kapier" && computerSelection === "nożyce") {
+		return "You lose! Nożyce pokonują papier.";
+	} else if (playerSelection === "nożyce" && computerSelection === "papier") {
+		return "You win! Nożyce pokonują papier.";
+	} else if (playerSelection === "nożyce" && computerSelection === "kamień") {
+		return "You lose! Kamień pokonuje nożyce.";
+	} else if (
+		(playerSelection === "nożyce" && computerSelection === "nożyce") ||
+		(playerSelection === "kamień" && computerSelection === "kamień") ||
+		(playerSelection === "papier" && computerSelection === "papier")
+	) {
+		return "Remis";
+	} else {
+		return "Wprowadź: kamień lub papier lub nożyce.";
+	}
+}
+
+const playerSelection = "kAmień";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));

30597ae530746f3021972519de233bbd6472a959
marczik <marek99928@wp.pl>
2022-08-11 15:20:05 +0200

    Create plan

---
+++ javascript.js
@@ -0,0 +1,15 @@
+/*
+Create function getComputerChoice, return rundom "Kamień", "Papier", "Nożyczki"
+
+Create function playRound - 1 round game; 
+ - parameters: playerSelection and computerSelection
+ - return: "Przegrałeś! Papier bije Kamień"
+ - make function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation).
+
+Create function game
+ - call the playRound inside to play 5 round game that keeps score and reports a winner or loser at the end.
+ - use loop to play those 5 rounds
+ 
+ Use prompt() to get input from the user
+*/
+
